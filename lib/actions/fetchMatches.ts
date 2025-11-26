e2fe12678ccdd4ca47cbfcfc50945c5023ecab4a
Baranyi Gábor <bgabor91@freemail.hu>
2025-11-26 16:43:31 +0100

    feat: enhance MatchList and Pagination components with improved data fetching and pagination logic

--- lib/actions/fetchMatches.ts
+++ lib/actions/fetchMatches.ts
@@ -1,14 +1,39 @@
-'use server'
+"use server";
 import connectDB from "@/config/database";
 import Match from "@/models/Match";
+import { Match as MatchType } from "@/types/types";
 import { convertToJSON } from "../utils/convertToJSON";
 
-export const fetchMatches = async () => {
+interface IFetchMatchesProps {
+  limit?: number;
+  skip?: number;
+}
+
+export const fetchMatches = async ({
+  limit = 0,
+  skip = 0,
+}: IFetchMatchesProps = {}) => {
   await connectDB();
   try {
-    const matches = await Match.find().lean();
+    const matches = await Match.find()
+      .sort({ date: -1 })
+      .limit(limit)
+      .skip(skip)
+      .lean()
+      .exec();
     if (!matches) return null;
     return convertToJSON(matches);
+  } catch (error) {
+    console.error(error);
+    return new Error(error instanceof Error ? error.message : String(error));
+  }
+};
+
+export const fetchMatchesCount = async () => {
+  await connectDB();
+  try {
+    const matchesCount = await Match.countDocuments({}).exec();
+    return matchesCount;
   } catch (error) {
     console.error(error);
     return new Error(error instanceof Error ? error.message : String(error));
