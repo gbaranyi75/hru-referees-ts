7dc5f97c2c42864e942f8846a83be39728cf7f78
Baranyi Gábor <bgabor91@freemail.hu>
2026-01-29 19:48:27 +0100

    refactor: enhance IUser interface and restructure User schema definition for clarity

--- models/User.ts
+++ models/User.ts
@@ -1,6 +1,5 @@
-import { Schema, model, models, Model } from "mongoose";
+import { Schema, model, models, Model, SchemaDefinition } from "mongoose";
 
-// Opcionális, de erősen ajánlott: definiáljunk egy interfészt a felhasználóhoz
 export interface IUser {
   email: string;
   clerkUserId: string;
@@ -19,63 +18,60 @@ export interface IUser {
   updatedAt?: Date;
 }
 
-const UserSchema = new Schema<IUser>(
-  {
-    email: {
-      type: String,
-      unique: [true, "Email already exists"],
-      required: [true, "Email is required"],
-    },
-    clerkUserId: {
-      type: String,
-      unique: true,
-      required: [true, "ClerkUserId is required"],
-    },
-    username: {
-      type: String,
-      required: [true, "Username is required"],
-      unique: [true, "Username already exists"],
-    },
-    image: {
-      type: String,
-    },
-    address: {
-      city: {
-        type: String,
-        required: false,
-        default: "",
-      },
-      country: {
-        type: String,
-        required: false,
-        default: "",
-      },
-    },
-    phoneNumber: {
+const userSchemaDefinition: SchemaDefinition<IUser> = {
+  email: {
+    type: String,
+    unique: true,
+    required: [true, "Email is required"],
+  },
+  clerkUserId: {
+    type: String,
+    unique: true,
+    required: [true, "ClerkUserId is required"],
+  },
+  username: {
+    type: String,
+    required: [true, "Username is required"],
+    unique: true,
+  },
+  image: {
+    type: String,
+  },
+  address: {
+    city: {
       type: String,
       required: false,
+      default: "",
     },
-    hasTitle: {
-      type: String,
-      required: true,
-    },
-    status: {
-      type: String,
-      required: true,
-    },
-    facebookUrl: {
+    country: {
       type: String,
       required: false,
-    },
-    instagramUrl: {
-      type: String,
-      required: false,
+      default: "",
     },
   },
-  {
-    timestamps: true,
+  phoneNumber: {
+    type: String,
+    required: false,
   },
-);
+  hasTitle: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+  facebookUrl: {
+    type: String,
+    required: false,
+  },
+  instagramUrl: {
+    type: String,
+    required: false,
+  },
+};
+
+const UserSchema = new Schema<IUser>(userSchemaDefinition, { timestamps: true });
 
 const User = (models.User as Model<IUser>) || model<IUser>("User", UserSchema);
 
